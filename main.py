7b7b7b6ea04d14f2da7d8a9019c2580031006c49
Molla Sazidur Rahman <rsazidur936@gmail.com>
2022-03-28 16:28:08 +0600

    Create wish me function to welcome author

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pyttsx3
+import datetime
 
 engine = pyttsx3.init("sapi5")
 
@@ -14,6 +15,18 @@ def speck(audio):
     engine.say(audio)
     engine.runAndWait()
 
+def wish_me():
+    hour = int(datetime.datetime.now().hour)
+    if 0 <= hour < 12:
+        speck(f"Good morning {author}")
+    elif 12 <= hour < 18:
+        speck(f"Good afternoon {author}")
+    else:
+        speck(f"Good evening {author}")
+
+    speck("Tell me how can i help you?")
+
 
 if __name__ == "__main__":
-    speck(f"Welcome {author}, I am Dante.")
+    # speck(f"Welcome {author}, I am Dante.")
+    wish_me()
